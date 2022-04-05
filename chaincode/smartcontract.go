33e8dd4467426646d1be1e234e1bb1f12c32d595
hiro942 <ee.Liulei@Gmail.com>
2022-04-05 23:15:24 +0800

    test

--- chaincode/smartcontract.go
+++ chaincode/smartcontract.go
@@ -16,9 +16,9 @@ type tci contractapi.TransactionContextInterface
 type Node2 struct {
 	Id            string      `json:"id"`
 	NodeType      string      `json:"nodeType"`
-	//PublicKey     interface{} `json:"publicKey"`
-	CreatedAt     time.Time   `json:"createdAt"`
-	UpdatedAt     time.Time   `json:"updatedAt"`
+	PublicKey     interface{} `json:"publicKey"`
+	//CreatedAt     time.Time   `json:"createdAt"`
+	//UpdatedAt     time.Time   `json:"updatedAt"`
 }
 
 type Node struct {
@@ -57,8 +57,8 @@ type UserAccessRecords map[NodePair][]UserAccessRecord
 //InitLedger2 initialize the ledger
 func (s *SmartContract) InitLedger2(ctx tci) error {
 	nodes := []Node2 {
-		{"user-1", "user", time.Now(), time.Now()},
-		{"satellite-1", "satellite",time.Now(), time.Now()},
+		{"user-1", "user", map[string]string{"macAddr1":"123", "macAddr2":"456"}},
+		{"satellite-1", "satellite", "123456"},
 	}
 	for _, node := range nodes {
 		nodeJSON, _ := json.Marshal(node)
